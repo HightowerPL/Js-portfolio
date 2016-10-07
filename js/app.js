ae77be6fcf7b0accb18f185e620f8d9f9f60471e
Jakub Snopkiewicz <Jakub Snopkiewicz>
2016-10-08 00:29:33 +0200

    Poprawiony nav na mobile

--- js/app.js
+++ js/app.js
@@ -1,5 +1,38 @@
 $(document).foundation();
 
+$(function(){
+       $("#form").submit(function(e){
+           var info=$("#info");
+           var form=$(this);
+           $.ajax({
+                url: "contact.php",
+                dataType: "JSON",
+                type: "post",
+                data:form.serialize(),
+                beforeSend: function(){
+                    info.hide();
+                },
+                success: function(obj){
+                    if (obj.type=="ok")
+                    {
+                        info.addClass("ok").removeClass("error").html(obj.text);
+                        form.get(0).reset();
+                    } else
+                    {
+                       info.addClass("error").removeClass("ok").html(obj.text);
+                    }
+                },
+                error : function(){
+                    info.addClass("error").removeClass("ok").html("Przepraszamy, ale wystąpił błąd podczas wysyłania informacji.")
+                },
+                complete: function(){
+                    info.show();
+                }
+           });
+           e.preventDefault();
+       })
+    })
+
 $(document).ready(function(){
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
